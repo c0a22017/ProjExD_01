e306a012e28130bd9dbfe3b0a32aeb9a32587478
c0a22017 <c0a2201755@edu.teu.ac.jp>
2023-11-21 16:36:27 +0900

    practice2

--- pygame_image2.py
+++ pygame_image2.py
@@ -6,13 +6,15 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#size
     clock  = pg.time.Clock()#time
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#practice1
+    bg_img01 = pg.image.load("ex01/fig/pg_bg.jpg")#practice1
+    bg_img02 = pg.image.load("sample/fig/3.png")#practice2
+    kk_img01 = pg.transform.flip(kk_img01,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img01, [0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
