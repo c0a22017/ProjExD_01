7e7a252020ad7f360a7058349d6b46685e80d858
c0a22017 <c0a2201755@edu.teu.ac.jp>
2023-11-21 16:52:40 +0900

    practice3,4,5

--- pygame_image2.py
+++ pygame_image2.py
@@ -6,15 +6,19 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#size
     clock  = pg.time.Clock()#time
-    bg_img01 = pg.image.load("ex01/fig/pg_bg.jpg")#practice1
+    bg_img01 = pg.image.load("sample/fig/pg_bg.jpg")#practice1
     bg_img02 = pg.image.load("sample/fig/3.png")#practice2
-    kk_img01 = pg.transform.flip(kk_img01,True,False)
+    bg_img01 = pg.transform.flip(kk_img01,True,False)#angle
+    kk_img01 = pg.transform.flip(kk_img01,True,False)#angle
+    kk_imgs = [kk_img01, pg.transform.rotozoom(kk_img01,6,1.0),pg.transform.rotozoom(kk_img01,10,1.0),pg.transform.rotozoom(kk_img01,4,1.0)]#practice3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img01, [0, 0])
+        screen.blit(bg_img01, [0, 0])#smooth
+        screen.blit(bg_img01, [-tmr, 0])#practice4
+        screen.blit(kk_imgs[tmr%2], [300,200])#practice5
         pg.display.update()
         tmr += 1        
         clock.tick(10)
