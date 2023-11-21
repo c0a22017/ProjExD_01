e2934dae48488495b282f214b2b2deaf5258d70b
c0a22017 <c0a2201755@edu.teu.ac.jp>
2023-11-21 17:10:18 +0900

    最終チェック終わり

--- pygame_image2.py
+++ pygame_image2.py
@@ -7,8 +7,8 @@ def main():
     screen = pg.display.set_mode((800, 600))#size
     clock  = pg.time.Clock()#time
     bg_img01 = pg.image.load("sample/fig/pg_bg.jpg")#practice1
-    bg_img02 = pg.image.load("sample/fig/3.png")#practice2
-    bg_img01 = pg.transform.flip(kk_img01,True,False)#angle
+    kk_img01 = pg.image.load("sample/fig/3.png")#practice2
+    bg_img02 = pg.transform.flip(bg_img01,True,False)#angle
     kk_img01 = pg.transform.flip(kk_img01,True,False)#angle
     kk_imgs = [kk_img01, pg.transform.rotozoom(kk_img01,6,1.0),pg.transform.rotozoom(kk_img01,10,1.0),pg.transform.rotozoom(kk_img01,4,1.0)]#practice3
     tmr = 0
@@ -16,13 +16,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img01, [0, 0])#smooth
-        screen.blit(bg_img01, [-tmr, 0])#practice4
+        screen.blit(bg_img01, [-tmr, 0])#practice4 smooth
         screen.blit(bg_img02, [1600-tmr, 0])#practice6
+        screen.blit(bg_img01, [3200-tmr, 0])#practice8
         screen.blit(kk_imgs[tmr%200//50], [300,200])#practice5,practice7
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
+        if(tmr>3199):
+            tmr=0
 
 
 if __name__ == "__main__":
